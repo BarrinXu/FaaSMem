303c847079706169fb4734003b6158aa2c1e1810
Barrin <58480159+BarrinXu@users.noreply.github.com>
2024-03-14 22:51:36 +0800

    Update install_rdma.sh

--- Fastswap/install_rdma.sh
+++ Fastswap/install_rdma.sh
@@ -4,5 +4,7 @@ set -e
 
 apt update
 apt install -y screen infiniband-diags opensm libibverbs-dev librdmacm-dev perftest rdma-core ibutils ibverbs-utils rdmacm-utils
+systemctl stop opensm.service
+systemctl disable opensm.service
 
 reboot
